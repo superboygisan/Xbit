e079a9a813f43ee517cbaaf7dbd180e407237b77
superboygisan <superboygisan@gmail.com>
2026-06-15 10:28:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # Vars For API End Pont.
-YTPROXY_URL = getenv("YTPROXY_URL", 'https://api.shrutibots.site') ## xBit Music Endpoint.
-YT_API_KEY = getenv("YT_API_KEY" , None ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
+SHRUTI_API_URL = getenv("SHRUTI_API_URL", 'https://api.shrutibots.site') ## xBit Music Endpoint.
+SHRUTI_API_KEY = getenv("SHRUTI_API_KEY" , None ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
 
 ## Other vaes
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
